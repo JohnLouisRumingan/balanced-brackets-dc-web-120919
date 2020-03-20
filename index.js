4d547fb7b75487d4d0d73bef1974817ccb1958b9
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-20 10:31:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
+function isBalanced (string) {
 
+    //create a process inside here that will return false, otherwise true
 
-function isBalanced () {
+    //returns false for obvious edge cases where a string cannot possibly be balanced
+    if(string.length % 2 === 1){
+        return false 
+    }
 
+    let isOpenParenthesis = 0;
+    let isOpenBracket = 0;
+    let isOpenCurly = 0;
 
-    return false 
+    for (let i=0; i<string.length-1; i++){
+        
+        if(string[i]===")"){
+          isOpenParenthesis -= 1;
+          if(isOpenParenthesis < 0){
+            return false
+          }
+        }
+        else if(string[i]==="]"){
+          isOpenBracket -= 1;
+          if(isOpenBracket < 0 ){
+            return false
+          }
+        }
+        else if(string[i]==="}"){
+          isOpenCurly -= 1;
+          if(isOpenCurly < 0){
+            return false
+          }
+        }
+
+        if(string[i]==="("){
+            isOpenParenthesis += 1;
+        }
+        else if(string[i]==="["){
+            isOpenBracket += 1;
+        }
+        else if(string[i]==="{"){
+            isOpenCurly += 1 ;
+        }
+        
+    }
+
+    return true  
 }
 
